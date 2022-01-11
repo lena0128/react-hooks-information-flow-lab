45e52dc37f3f4f759587e3e555ea60e78f1d064e
Lena Hou <lenahou2013@gmail.com>
2022-01-11 14:55:57 -0500

    commit tree b15c7329d7eef3da188093715f025a0538a46968

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Header(props){
+
+  return(
+    <header>
+        <h2>Shopster</h2>
+        <button onClick={props.onDarkModeClick}>
+          {props.isDarkMode ? "Dark" : "Light"} Mode
+        </button>
+      </header>
+  )
+
+}
+
+export default Header
